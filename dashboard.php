a138f8bf03b72c824b3a42e54edfa395363ed5b2
varun212001 <varunkumarwelcome2001@gmail.com>
2021-01-28 17:13:55 +0530

    first commit

--- dashboard.php
+++ dashboard.php
@@ -49,7 +49,7 @@
         <i class="tim-icons icon-bullet-list-67 visible-on-sidebar-mini text-muted"></i>
       </button>
         </div>
-        <?php include"aslide.php";?>
+        <?php include "aslide.php";?>
         <div class="main-panel">
             <?php include "nav.php";?>
             <div class="content">
@@ -619,113 +619,7 @@
                                     </div>
                                     </div>
                                     </div>
-                                    <!-- <div class="col-lg-12">
-                                        <div class="card">
-                                            <div class="card-header">
-                                                <h4 class="card-title">Global Sales by Top Locations</h4>
-                                                <p class="card-category">All products that were shipped</p>
-                                            </div>
-                                            <div class="card-body">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="table-responsive">
-                                                            <table class="table">
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/US.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>USA</td>
-                                                                        <td class="text-right">
-                                                                            2.920
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            53.23%
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/DE.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>Germany</td>
-                                                                        <td class="text-right">
-                                                                            1.300
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            20.43%
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/AU.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>Australia</td>
-                                                                        <td class="text-right">
-                                                                            760
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            10.35%
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/GB.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>United Kingdom</td>
-                                                                        <td class="text-right">
-                                                                            690
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            7.87%
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/RO.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>Romania</td>
-                                                                        <td class="text-right">
-                                                                            600
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            5.94%
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            <div class="flag">
-                                                                                <img src="assets/img/BR.png" alt="flag">
-                                                                            </div>
-                                                                        </td>
-                                                                        <td>Brasil</td>
-                                                                        <td class="text-right">
-                                                                            550
-                                                                        </td>
-                                                                        <td class="text-right">
-                                                                            4.34%
-                                                                        </td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6 ml-auto mr-auto">
-                                                        <div id="worldMap" style="height: 300px;"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div> -->
+                                    -->
                                     </div>
                                 </div>
                                 <?php include "foot.php";?>
---
+++ employee.php
@@ -0,0 +1,287 @@
+ -->
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png"> -->
+    <link rel="icon" type="image/png" href="assets/img/nutz.jpg">
+    <title>
+        Dashboard FOR NUTZ
+    </title>
+    <!-- Extra details for Live View on GitHub Pages -->
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://www.creative-tim.com/product/black-dashboard" />
+    <!--  Social tags      -->
+    <meta name="keywords" content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 4 dashboard, bootstrap 4, css3 dashboard, bootstrap 4 admin, black dashboard bootstrap 4 dashboard, frontend, responsive bootstrap 4 dashboard, black design, black dashboard bootstrap 4 dashboard">
+    <meta name="description" content="Black Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Black Dashboard PRO by Creative Tim">
+    <meta itemprop="description" content="Black Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+    <meta itemprop="image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/93/opt_bd_thumbnail.jpg">
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="product">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Black Dashboard PRO by Creative Tim">
+    <meta name="twitter:description" content="Black Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/93/opt_bd_thumbnail.jpg">
+    <!-- Open Graph data -->
+    <meta property="fb:app_id" content="655968634437471">
+    <meta property="og:title" content="Black Dashboard PRO by Creative Tim" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://demos.creative-tim.com/black-dashboard-pro/examples/dashboard.html" />
+    <meta property="og:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/93/opt_bd_thumbnail.jpg" />
+    <meta property="og:description" content="Black Dashboard PRO is a beautiful Bootstrap 4 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you."
+    />
+    <meta property="og:site_name" content="Creative Tim" />
+    <?php include "head.php"; ?>
+    <style>
+        .col-lg-77 {
+            position: relative;
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+        
+        @media (min-width: 992px) {
+            .col-lg-77 {
+                /* flex: 0 0 58.333333%; */
+                max-width: 98.333333%;
+            }
+        }
+    </style>
+</head>
+
+<body class="sidebar-mini ">
+    <div class="wrapper">
+        <div class="navbar-minimize-fixed">
+            <button class="minimize-sidebar btn btn-link btn-just-icon">
+        <i class="tim-icons icon-align-center visible-on-sidebar-regular text-muted"></i>
+        <i class="tim-icons icon-bullet-list-67 visible-on-sidebar-mini text-muted"></i>
+      </button>
+        </div>
+        <?php include "aslide.php";?>
+        <div class="main-panel">
+        <?php include "nav.php";?>
+            <div class="content">
+                <div class="row">
+                  
+                <div class="col-lg-77">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="tools float-right">
+                                <div class="dropdown">
+                                    <button type="button" class="btn btn-link dropdown-toggle btn-icon" data-toggle="dropdown">
+                      <i class="tim-icons icon-settings-gear-63"></i>
+                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a button type="button" class="dropdown-item" data-toggle="modal" data-target="#exampleModal" href="#pablo">Add user</a>
+                                       
+                                    </div>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+
+
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                 
+                                                <span aria-hidden="true">&times;</span>
+                                              </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    ...
+                                                </div>
+                                                <div class="modal-footer">
+                                                   
+                                                    <button type="button" class="btn btn-primary">Submit</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <h5 class="card-title">Employee Table</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class="text-primary">
+                                        <tr>
+                                            <th class="text-center">
+                                                #
+                                            </th>
+                                            <th>
+                                                Name
+                                            </th>
+                                            <th>
+                                                Job Position
+                                            </th>
+                                            
+                                            <th class="text-right">
+                                                Salary
+                                            </th>
+                                            <th class="text-right">
+                                                Actions
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/tania.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Tania Mike
+                                            </td>
+                                            <td>
+                                                Develop
+                                            </td>
+                                            
+                                            <td class="text-right">
+                                                € 99,225
+                                            </td>
+                                            <td class="text-right">
+                                               
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-icon btn-sm btn-simple" data-original-title="Tooltip on top" title="Delete">
+                            <i class="tim-icons icon-simple-remove"></i>
+                          </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/robi.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                John Doe
+                                            </td>
+                                            <td>
+                                                CEO
+                                            </td>
+                                           
+                                            <td class="text-right">
+                                                € 89,241
+                                            </td>
+                                            <td class="text-right">
+                                                
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm btn-icon btn-simple" data-original-title="Tooltip on top" title="Delete">
+                            <i class="tim-icons icon-simple-remove"></i>
+                          </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/lora.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Alexa Mike
+                                            </td>
+                                            <td>
+                                                Design
+                                            </td>
+                                           
+                                            <td class="text-right">
+                                                € 92,144
+                                            </td>
+                                            <td class="text-right">
+                                               
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-icon btn-sm btn-simple" data-original-title="Tooltip on top" title="Delete">
+                            <i class="tim-icons icon-simple-remove"></i>
+                          </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/jana.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Jana Monday
+                                            </td>
+                                            <td>
+                                                Marketing
+                                            </td>
+                                           
+                                            <td class="text-right">
+                                                € 49,990
+                                            </td>
+                                            <td class="text-right">
+                                               
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm btn-icon" data-original-title="Tooltip on top" title="Delete">
+                            <i class="tim-icons icon-simple-remove"></i>
+                          </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/mike.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Paul Dickens
+                                            </td>
+                                            <td>
+                                                Develop
+                                            </td>
+                                           
+                                            <td class="text-right">
+                                                € 69,201
+                                            </td>
+                                            <td class="text-right">
+                                              
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm btn-icon" data-original-title="Tooltip on top" title="Delete">
+                            <i class="tim-icons icon-simple-remove"></i>
+                          </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">
+                                                <div class="photo">
+                                                    <img src="assets\img/emilyz.jpg" alt="photo">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                Manuela Rico
+                                            </td>
+                                            <td>
+                                                Manager
+                                            </td>
+                                            
+                                            <td class="text-right">
+                                                € 99,201
+                                            </td>
+                                            <td class="text-right">
+                                                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm btn-icon" data-original-title="Tooltip on top" title="Delete">
+                                                    <i class="tim-icons icon-simple-remove"></i>
+                                                  </button>
+                                              
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+        <?php include "foot.php";?>
+</body>
+
+
+
+
+</html>
